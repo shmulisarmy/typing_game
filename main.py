5eb787eb4513d2eda52f383ce46425603c783a84
shmuli <shmulikeller@gmail.com>
2024-03-27 23:38:38 -0400

    added costume getters and setters

--- main.py
+++ main.py
@@ -4,29 +4,77 @@ from flask import Flask, render_template, session, request
 app = Flask(__name__)
 app.secret_key = b'\xfd\xec\x82\x96\x94\xa2\xb0\xd3\xb7\x15\xe0\x8e\xd3\x1c\xb7\x1a'
 
+def secure(password: str) -> bool:
+    return len(password) < 8
+
+
+
+@app.route('/signup')
+def signup():
+    name = request.args.get("name", type=str)
+    password = request.args.get("password", type=str)
+    
+    if name in accounts:
+        return "this username already exists"
+    
+    if not secure(password):
+        return "this password is not secure please create another one"
+    
+    accounts[name] = password
+    accountInfo[name]["wpms"] = session.get("wpms")
+    accountInfo[name]["levelUpTo"] = session.get("levelUpTo")
+    del session["wpms"]
+    del session["levelUpTo"]
+    session["username"] = name
+
+    
+def getter(session, *attrs) -> list:
+    """
+    funcType = (non decorator) wrapper
+    takes in a session and attributes that a 
+    user wants to get either from accountInfo or session
+    """
+    username = session.get("username")
+    if username:
+        return [accountInfo[username][attr] for attr in attrs]
+    return [session.get(attr) for attr in attrs]
+
+def setter(session, attr, value) -> list:
+    """
+    funcType = (non decorator) wrapper
+    takes in a session and one attribute that the
+    user wants to set either into accountInfo or session
+    """
+    username = session.get("username")
+    if username:
+        accountInfo[username][attr] = value
+    session[attr] = value
+
+
+
 
 @app.route('/')
 def main():
     print(f"{session}")
-    if not session.get("wpms"):
-        session["wpms"] = [0 for i in range(100)]
-    levelUpTo = session.get("levelUpTo", 0)
+    wpms, levelUpTo = getter(session, "wpms", "levelUpTo")
+    if not wpms:
+        setter(session, "wpms", 0)
     return render_template("main.html", sentence=typingLevels[levelUpTo], level=levelUpTo)
 
 @app.route("/showLevels")
 def showLevels():
-    allLevels = session.get("wpms")
-    if not allLevels:
-        session["wpms"] = [0 for i in range(100)]
+    [allLevelsWithWpms] = getter(session, "wpms")
+    if not allLevelsWithWpms:
+        setter(session, "wpms", [0 for _ in range(100)])
 
-    print(f"{allLevels = }")
-    return render_template("displayLevels.html", allLevels=allLevels)
+    print(f"{allLevelsWithWpms = }")
+    return render_template("displayLevels.html", allLevels=allLevelsWithWpms)
 
 @app.route("/gotoLevel/<int:level>")
 def gotoLevel(level: int):
-    print(f"got to level {level}")
+    print(f"go to level {level}")
     print(f"{session = }")
-    session["levelUpTo"] = level
+    setter(session, "levelUpTo", level)
     return render_template("main.html", sentence=typingLevels[level], level=level)
 
 
@@ -35,10 +83,12 @@ def loadNextLevel(level: int):
     print(f"load level after {level}")
     wpm = request.args.get("wpm", type=int)
     assert isinstance(wpm, int)
-    session["wpms"][level-1] = wpm
+    [wpms] = getter(session, "wpms")
+    print(f"{wpms}")
+    wpms[level-1] = wpm
     level+=1
     print(f"level updated to = {level}")
-    session["levelUpTo"] = level
+    setter(session, "levelUpTo", level)
     return {"sentence": typingLevels[level], "level": level}
 
 
@@ -66,6 +116,14 @@ typingLevels = [
     "Language: English",
     "*** START OF THIS PROJECT GUTENBERG EBOOK THE COMPLETE WORKS OF WILLIAM SHAKESPEARE ***",
 ]
+
+accounts = {
+
+}
+
+accountInfo = {
+
+}
 
 if __name__ == '__main__':
     app.run(debug=True)
